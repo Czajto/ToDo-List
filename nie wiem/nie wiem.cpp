430aad92c76eb2e61c849e18b52ce38530435274
Czajto <rogala10009@gmail.com>
2025-03-03 20:02:59 +0100

    Kolejne zrobione pozdro :)

--- nie wiem/nie wiem.cpp
+++ nie wiem/nie wiem.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <limits>
 #include <cstdlib>  // dla system()
 
 /*
@@ -13,17 +14,22 @@ X = NIE ZROBIONE
 RZECZY DO ZROBIENIA!!!!
 ##############################
 MOZLIWOSC EDYTOWANIA PODANYCH ZADAN Z LISTY (X)
-DODAWANIE A NIE NADPISYWANIE DO ISTNIEJACYCH PLIKOW Z ZADANIAMI (X)
 WSZYSTKO MA LADNIE SIE WYSWIETLAC (X)
 DODANIE FUNKCJI OZNACZANIA W DANYM PLIKU RZECZY ZROBIONYCH I NIE (X)
+FUNKCJA KTORA CZYSCI DANE ZADANIA Z PLIKU (X)
+FUNKCJA KTORA USUWA PLIKI Z KOMPUTERA (X)
 
 
+MOZLIWOSC CZYSCZENIA OBECNEJ LISTY Z FUNKCJA "CZY NA PEWNO?" (✓)
+DODAWANIE A NIE NADPISYWANIE DO ISTNIEJACYCH PLIKOW Z ZADANIAMI (✓)
 ############################
 */
 
-
 using namespace std;
 namespace fs = filesystem;
+
+// Deklaracja funkcji potwierdzenia
+bool potwierdzenie(const string& komunikat);
 
 void czyscEkran() {
     // Sprawdzenie systemu operacyjnego
@@ -33,7 +39,6 @@ void czyscEkran() {
     system("clear");  // dla Linux/macOS
 #endif
 }
-
 int wybor() {
     int wybor;
 
@@ -47,7 +52,6 @@ int wybor() {
     cin.ignore();
     return wybor;
 }
-
 void dodajZadanie(vector<string>& lista) {
     string zadanie;
     cout << "Podaj tresc zadania" << endl;
@@ -55,7 +59,6 @@ void dodajZadanie(vector<string>& lista) {
     lista.push_back(zadanie);
     cout << "Dodano zadanie: " << "\"" << zadanie << "\"" << endl;
 }
-
 void pokazywanieZadania(const vector<string>& lista) {
     if (lista.empty())
     {
@@ -63,15 +66,15 @@ void pokazywanieZadania(const vector<string>& lista) {
         return;
     }
 
-    cout << "\nTwoje zadania:" << endl;
+    cout << "Twoje zadania:" << endl;
 
     // Iteruje po kolei kazde zadanie z listy i je wyswietla
     for (size_t i = 0; i < lista.size(); i++)
     {
         cout << i + 1 << ". " << lista[i] << ".\n";
     }
+    cout << endl;
 }
-
 void usuwanieZadania(vector<string>& lista) {
     if (lista.empty())
     {
@@ -92,11 +95,10 @@ void usuwanieZadania(vector<string>& lista) {
     cout << "Usunieto zadanie: " << "\"" << lista[index - 1] << "\"!" << endl;
     lista.erase(lista.begin() + index - 1);
 }
-
 void zapiszDoPliku(const vector<string>& lista, string& nazwa) {
     string sciezka = "C:\\Users\\rogal\\Desktop\\To-Do Listy\\" + nazwa + ".txt";
 
-    ofstream plik(sciezka);
+    ofstream plik(sciezka, ios::app);
 
     if (!plik)
     {
@@ -112,13 +114,10 @@ void zapiszDoPliku(const vector<string>& lista, string& nazwa) {
     plik.close();
     cout << "Zadania zostaly zapisane do pliku" << endl;
 }
-
-
 void wczytywanieZpliku(vector<string> &lista, string &nazwa) {
     string sciezka = "C:\\Users\\rogal\\Desktop\\To-Do Listy\\" + nazwa + ".txt";
 
     ifstream plik(sciezka);
-
 
         if (!plik) {
             cout << "Brak pliku lub blad otwierania" << endl;
@@ -126,8 +125,6 @@ void wczytywanieZpliku(vector<string> &lista, string &nazwa) {
     }
         lista.clear();
         string zadanie;
-
-        
 
         while (getline(plik, zadanie))
         {
@@ -138,7 +135,6 @@ void wczytywanieZpliku(vector<string> &lista, string &nazwa) {
         cout << "Zadania wczytane z pliku: " << "\"" << nazwa << ".txt\"" << endl;
 
 }
-
 void wyswietlaniePlikow(const string& sciezka) {
     cout << "Pliki w folderze: " << sciezka << endl;
 
@@ -147,38 +143,81 @@ void wyswietlaniePlikow(const string& sciezka) {
         cout << " - " << plik.path().stem() << endl;
     }
 }
+void wyczyscCalaListe(vector<string>&lista) {
+    if (lista.empty())
+    {
+        cout << "Brak zadan na liscie!" << endl;
+        return;
+    }
+
+    if (potwierdzenie("Czy na pewno chcesz usunac wszystkie zadania z listy?: ")) {
+        lista.clear();
+        cout << "Wszystkie zadania zostaly usuniete. " << endl;
+    }
+    else {
+        cout << "Anulowano operacje." << endl;
+    }
+
+    // Czyszczenie bufora wejścia dla pewności
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+
+}
+bool potwierdzenie(const string& komunikat) {
+    char decyzja;
+    cout << komunikat << " (T/N): ";
+    cin >> decyzja;
+
+    //Konwersja do wielkiej litery, aby działało dla "t" i "T"
+    decyzja = toupper(decyzja);
+
+    return (decyzja == 'T');  // Zwraca true, jeśli użytkownik wpisał 'T'
+}
 
 int main() {
     string nazwaPliku;
     vector<string> listaZadan;
     string folder = "C:\\Users\\rogal\\Desktop\\To-Do Listy";  // Ustaw ścieżkę folderu
+    int wyborUsuniecia;
+
 
     while (true) {
 
         int mainWybor = wybor();
         switch (mainWybor) {
-        case 1: // DODAWANIE ZADAN
+        case 1:                             // DODAWANIE ZADAN
             czyscEkran();  // Wyczyszczenie ekranu tylko przed pokazaniem zadań
 
 
             dodajZadanie(listaZadan);
+
             break;
         case 2: // POKAZYWANIE ZADAN
             czyscEkran();  
-
-
             pokazywanieZadania(listaZadan);
             break;
-        case 3: // USUWANIE ZADAN
+        case 3:                             // USUWANIE ZADAN
             czyscEkran();  
+            cout << "Co chcesz zrobic?" << endl;
+            cout << "[1] Usunac wszystkie zadania z listy " << endl;
+            cout << "[2] Usunac pojedyncze zadania z listy wybrane przez ciebie" << endl;
+            cin >> wyborUsuniecia;
+            if (wyborUsuniecia == 1) {
+                wyczyscCalaListe(listaZadan);
+            }
+            else if (wyborUsuniecia == 2) {
+                pokazywanieZadania(listaZadan);
+                usuwanieZadania(listaZadan);
 
-            // Pokazuje obecne zadania w liscie
-            pokazywanieZadania(listaZadan);
+            }
+            else {
+                cout << "wybierz jedna z opcji";
+                return 0;
+            }
 
-            // Usuwa zadania z listy
-            usuwanieZadania(listaZadan);
             break;
-        case 4: // ZAPISYWANIE ZADAN W PLIKU
+        case 4:                            // ZAPISYWANIE ZADAN W PLIKU
             // Wyświetlanie dostępnych plików przed zapisaniem
             wyswietlaniePlikow(folder);
 
@@ -188,7 +227,7 @@ int main() {
             // Zapisuje zadania i tworzy dla nich pliki
             zapiszDoPliku(listaZadan, nazwaPliku);
             break;
-        case 5: // WCZYTYWANIE Z PLIKU
+        case 5:                             // WCZYTYWANIE Z PLIKU
             cout << "Z którego pliku chcesz zaladowac zadania?" << endl;
 
             wyswietlaniePlikow(folder);
